1808579612afd17de1d78b876da002068b8f6f71
Caro <carolina.segovia.102@mi.unc.edu.ar>
2025-09-09 17:30:27 -0300

    ejerciciostp1

---
+++ practico1_astrometria/ejercicio18.py
@@ -0,0 +1,13 @@
+# %%
+
+import numpy as np
+import matplotlib.pyplot as plt
+from funciones import generador_congruencia_lineal_enteros
+from funciones import generador_congruencia_lineal
+
+# %%
+
+generador_congruencia_lineal_enteros(1000,10)
+prunt(i,x)
+
+# %%
