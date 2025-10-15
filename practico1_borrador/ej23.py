6f39aded04ce0658ea375648d745dd33f54dc0da
Caro <carolina.segovia.102@mi.unc.edu.ar>
2025-10-15 16:01:54 -0300

    tp3_terminado

--- practico1_borrador/ej23.py
+++
@@ -1,7 +0,0 @@
-#%%
-
-import matplotlib.pyplot as plt
-from mis_funciones import generador_galaxias
-import numpy as np
-from time import sleep
-# %%
--- practico1_borrador/p18.py
+++
@@ -1,4 +0,0 @@
-from mis_funciones import suma
-
-a = suma(2,3) #d y e las habiamos definido opcionales
-print(a)
